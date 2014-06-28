088d9758051d2c92c58ac7cf547c895ccb3c91dd
Jeff McRiffey <jeff.mcriffey@ambition.com>
2014-06-27 21:02:52 -0400

    update interface

--- flux-tools.js
+++ flux-tools.js
@@ -353,27 +353,18 @@ module.exports = Dispatcher;
 
 var Dispatcher = require('../dispatcher/Dispatcher');
 var EventEmitter = require('events').EventEmitter;
-var _uid = Date.now();
 
 /**
  * @constructor
  * Creates a new store.
+ * @param {[*]} initialData - An array of initial data.
  */
-var Store = function() {
+var Store = function(initialData) {
     this._emitter = new EventEmitter(); /** @private */
-    this._data = []; /** @private */
+    this._data = Array.isArray(initialData) ? initialData : []; /** @private */
+
     this.initActions();
-
-    //register the store with the dispatcher
-    Dispatcher.register(function(action, data) {
-        if (this._emitter.listeners(action).length) {
-            this._emitter.emit(action, data);
-
-            if (action !== 'change') { //emit change for other listeners
-                this._emitter.emit('change', data);
-            }
-        }
-    }.bind(this));
+    registerStore(this);
 };
 
 /**
@@ -395,168 +386,93 @@ Store.prototype.initActions = function() {
  * @returns {Object} - A handler for removing the listener.
  */
 Store.prototype.on = function(name, callback) {
-    this._emitter.addListener(name, callback); //add the listener
+    this._emitter.addListener(name, callback);
 };
 
 /**
  * @method un
  * Removes an event listener.
- * @param {String} name - The name of the subscription to cancel.
+ * @param {String} name - The name of the listener to cancel.
  */
 Store.prototype.un = function(name, callback) {
     this._emitter.removeListener(name, callback);
 };
 
 /**
- * @method add
- * Adds a datum to the store.
- * @param {Object} record - The record to add.
- * @param {*} record.data - The data to store.
- * @param {Number|String} record.id[record.id=_uid++] - A unique id for the data.
- * @param {Number} index[index=this.getCount()] - The position to insert the data.
- * @returns {Object|undefined} - The record or undefined.
+ * @method create
+ * Adds a record to the store.
+ * @param {Object} data - The data to add.
+ * @param {Number} index[index=this.count()] - The position to insert the data.
  */
-Store.prototype.add = function(record, index) {
-    var id = record.id !== undefined ? record.id : (_uid++).toString(36);
-
-    if (record.data !== undefined) {
-        this._data.splice(index !== undefined ? index : this.getCount(), 0, {
-            data: record.data, id: id
-        });
-
-        return record;
-    }
-
-    return;
+Store.prototype.create = function(data, index) {
+    this._data.splice(isNaN(index) ? this.count() : index, 0, data);
 };
 
 /**
- * @method load
- * Loads in a set of records. Clears the store by default.
- * @param {[Object]} records - An array of records to add to the store.
- * @params {Boolean} append[append=false] - True to append the records instead of replacing records.
- * @returns {Number} - The number of records successfully added.
+ * @method filter
+ * Finds data using the given function.
+ * Finds all data that matches the predicate
+ * @param {Function} fn - The predicate used to find items.
+ * @returns {[*]} - An array of matched data.
  */
-Store.prototype.load = function(records, append) {
-    var added = 0;
-    append = append !== undefined ? append : false;
-
-    if (!append) {
-        this.removeAll();
-    }
-
-    for (var i = 0, len = records.length; i < len; i++) {
-        if (this.add(records[i])) {
-            added++;
-        }
-    }
-
-    return added;
+Store.prototype.filter = function(fn) {
+    return this._data.filter(fn);
 };
 
 /**
- * @method findOne
- * Finds and returns all data using the given predicate.
- * @param {Function} fn - The fn used to find an item.
- * @returns {Object} - An array of matched data.
- */
-Store.prototype.find = function(fn) {
-    var records = [];
-
-    for (var i = 0, len = this.getCount(); i < len; i++) {
-        if (fn(this._data[i])) {
-            records.push(this._data[i]);
-        }
-    }
-
-    return records;
-};
-
-/**
- * @method findById
- * Finds and returns a datum by the given id.
- * @param {Number|String} id - The id to find.
- */
-Store.prototype.findById = function(id) {
-    return this.findOne(function(record) {
-        return record.id === id;
-    });
-};
-
-/**
- * @method findOne
- * Finds and returns a datum using the given function.
- * @param {Function} fn - The fn used to find an item.
- * @returns {Object} - The first datum matched.
- */
-Store.prototype.findOne = function(fn) {
-    for (var i = 0, len = this.getCount(); i < len; i++) {
-        if (fn(this._data[i])) {
-            return this._data[i];
-        }
-    }
-};
-
-/**
- * @method remove
- * Removes data from the store using the given function.
+ * @method destroy
+ * Removes data using the given function.
  * Removes all data that matches the predicate.
- * @param {Function} fn - The fn used to remove an item.
+ * @param {Function} fn - The predicate used to remove an item.
  */
-Store.prototype.remove = function(fn) {
-    var removed = 0;
-
-    while (this.removeOne(fn)) {
-        removed++;
-    }
-
-    return removed;
-};
-
-/**
- * @removeById
- * Removes a datum from the store by id.
- * @param {Number|String} id - The id to remove.
- */
-Store.prototype.removeById = function(id) {
-    return this.removeOne(function(record) {
-        return record.id === id;
+Store.prototype.destroy = function(fn) {
+    this._data = this._data.filter(function(value, i, arr) {
+        return !fn(value, i, arr);
     });
 };
 
 /**
- * @method removeOne
- * Removes a single datum from the store using the given function.
- * Removes the first datum that matches the predicate.
- * @param {Function} fn - The fn used to remove an item.
+ * @method all
+ * Gets an array of all data from the store.
+ * @returns [*] - All the store's data.
  */
-Store.prototype.removeOne = function(fn) {
-    for (var i = 0, len = this.getCount(); i < len; i++) {
-        if (fn(this._data[i])) {
-            this._data.splice(i, 1);
-            return true;
-        }
-    }
-
-    return false;
+Store.prototype.all = function() {
+    return this._data.slice();
 };
 
 /**
- * @method removeAll
+ * @method empty
  * Removes all data from the store.
  */
-Store.prototype.removeAll = function() {
+Store.prototype.empty = function() {
     this._data = [];
 };
 
 /**
- * @method getCount
+ * @method count
  * Gets the number of data items in the store.
  * @returns {Number}
  */
-Store.prototype.getCount = function() {
+Store.prototype.count = function() {
     return this._data.length;
 };
+
+/**
+ * @private
+ * @method registerStore
+ * Registers the store with the Dispatcher.
+ */
+function registerStore(store) {
+    Dispatcher.register(function(action, data) {
+        if (store._emitter.listeners(action).length) {
+            store._emitter.emit(action, data);
+
+            if (action !== 'change') { //emit change for other listeners
+                store._emitter.emit('change', data);
+            }
+        }
+    }.bind(store));
+}
 
 module.exports = Store;
 
--- src/store/tests/StoreTest.js
+++ src/store/tests/StoreTest.js
@@ -31,129 +31,57 @@ describe('Store', function() {
         assert.equal(changeCount, 4);
     });
 
-    it('should add data', function() {
+    it('should create data', function() {
         var store = new Store();
 
-        store.add({
-            data: 'mock data 1'
-        });
-        assert.equal(store.getCount(), 1);
+        store.create('mock data 1');
+        assert.equal(store.count(), 1);
+        assert.equal(store.all()[0], 'mock data 1');
 
-        store.add({
-            data: 'mock data 2',
-            id: 1
-        });
-        assert.equal(store.getCount(), 2);
+        store.create('mock data 2');
+        assert.equal(store.count(), 2);
+        assert.equal(store.all()[0], 'mock data 1');
+        assert.equal(store.all()[1], 'mock data 2');
 
-        store.add({
-            data: 'mock data 3'
-        }, 0);
-        assert.equal(store.getCount(), 3);
+        store.create('mock data 3', 0);
+        assert.equal(store.count(), 3);
+        assert.equal(store.all()[0], 'mock data 3');
+        assert.equal(store.all()[1], 'mock data 1');
+        assert.equal(store.all()[2], 'mock data 2');
     });
 
-    it('should load data', function() {
-        var store = new Store();
+    it('should filter data', function() {
+        var records;
+        var store = new Store([
+            'mock data 1',
+            'mock data 2',
+            'mock data 3'
+        ]);
 
-        store.add({
-            data: '1'
+        records = store.filter(function(data) {
+            return data === 'mock data 2';
         });
-        assert.equal(store.getCount(), 1);
 
-        store.load([{
-            id: 1234,
-            data: '2'
-        }, {
-            id: 5678,
-            data: '3'
-        }, {
-            badData: '5'
-        }]);
-        assert.equal(store.getCount(), 2);
-        assert.deepEqual(store._data[0], {data: '2', id: 1234});
-        assert.deepEqual(store._data[1], {data: '3', id: 5678});
-
-        store.load([{id: 9012, data: '4'}], true);
-        assert.equal(store.getCount(), 3);
-        assert.deepEqual(store._data[2], {data: '4', id: 9012});
+        assert.equal(records.length, 1);
+        assert.equal(records[0], 'mock data 2');
     });
 
     it('should remove data', function() {
-        var store = new Store();
+        var store = new Store(['mock data 1', 'mock data 2', 'mock data 1', 'mock data 3']);
 
-        store.load([{
-            data: 'mock data 1'
-        }, {
-            data: 'mock data 2',
-            id: 1
-        }, {
-            data: 'mock data 3',
-            id: 1
-        }, {
-            data: 'mock data 4'
-        }, {
-            data: 'mock data 4'
-        }, {
-            data: 'mock data 4'
-        }]);
-
-        store.removeById(1);
-        assert.equal(store.getCount(), 5);
-
-        store.removeOne(function(record) {
-            return record.data === 'mock data 4';
+        assert.equal(store.count(), 4);
+        store.destroy(function(data) {
+            return data === 'mock data 1';
         });
-        assert.equal(store.getCount(), 4);
-
-        store.remove(function(record) {
-            return record.data === 'mock data 4';
-        });
-        assert.equal(store.getCount(), 2);
-
-        store.removeAll();
-        assert.equal(store.getCount(), 0);
+        assert.equal(store.count(), 2);
     });
 
-    it('should find items', function() {
-        var store = new Store();
-        var result;
+    it('should empty data', function() {
+        var store = new Store(['mock data 1']);
 
-        store.load([{
-            data: 'mock data 1'
-        }, {
-            data: 'mock data 2',
-            id: 1
-        }, {
-            data: 'mock data 3'
-        }, {
-            data: 'mock data 4',
-            id: 5
-        }, {
-            data: 'mock data 4',
-            id: 6
-        }, {
-            data: 'mock data 4',
-            id: 7
-        }]);
-
-        result = store.findById(1);
-        assert.deepEqual(result, {data: 'mock data 2', id: 1});
-
-        result = store.findById(10);
-        assert.isUndefined(result);
-
-        result = store.find(function(record) {
-            return record.data === 'mock data 4';
-        });
-        assert.deepEqual(result, [{
-            data: 'mock data 4',
-            id: 5
-        }, {
-            data: 'mock data 4',
-            id: 6
-        }, {
-            data: 'mock data 4',
-            id: 7
-        }]);
+        assert.equal(store.count(), 1);
+        store.empty();
+        assert.equal(store.count(), 0);
     });
 });
 
