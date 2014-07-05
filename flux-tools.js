408099ee7c8512f58f9ccf44d566065c43f1deb3
Jeff McRiffey <jeff.mcriffey@ambition.com>
2014-07-05 01:15:15 -0400

    build

--- flux-tools.js
+++ flux-tools.js
@@ -6,7 +6,6 @@ var Dispatcher = require('./src/dispatcher/Dispatcher');
 var RemoteStore = require('./src/store/RemoteStore');
 var Store = require('./src/store/Store');
 
-
 global.window.FluxTools = {
     Dispatcher: Dispatcher,
     RemoteStore: RemoteStore,
